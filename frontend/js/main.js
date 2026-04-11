fbfee2e8d0256fabca5cdf84ac70928896d46664
cjbpcPortatil <bryan_andres_56@hotmail.com>
2026-04-11 19:05:30 +0200

    Se realizaron ajustes en los estilos y se inicio las variables en en main.js

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,3 +1,26 @@
 window.addEventListener("load",()=>{
-    
+    //variables elementos del DOM
+    const formTask = document.querySelector(".layout_form");
+    const inputTask = document.querySelector("#input_task");
+    const inputDate = document.querySelector("#input_date");
+    const tasksContainer = document.querySelector(".layout_tasks");
+
+    //variables para almacenar las tareas
+    let tasks = [];
+    let id = 0;
+
+    //Guardar tarea
+    formTask.addEventListener("submit",(e)=>{
+        e.preventDefault(); 
+        saveTask();        
+        });
 })
+
+function saveTask(){
+    if(inputTask.value.trim() !== " inputDate.values.trim() !== "){
+        let newTask = {
+            id: ++ id,
+        }
+
+    }
+}
